9523f8a52af4c619c68e49a64a31c2be73d7f27c
Sergio <sergio.piza@usp.br>
2019-05-13 22:09:34 -0300

    curso aula 40

---
+++ parNomeValor.js
@@ -0,0 +1,21 @@
+const saudacao = "Opa"
+
+function exec() {
+    const saudacao = "Alo"
+    return (saudacao)
+}
+
+console.log(saudacao)
+console.log(exec())
+
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: "Rua",
+        numero: 123
+    }
+}
+
+console.log(cliente);
