45b74821d3de0ca385be49f7f17b0f5252a1d0e7
Sergio <sergio.piza@usp.br>
2019-05-13 23:44:47 -0300

    curso aula 71

--- desestructuring.js
+++ desestructuring.js
@@ -12,3 +12,9 @@ console.log(nome, idade)
 
 const {nome: n, idade: i} = pessoa
 console.log(n, i)
+
+const {sobrenome, test = true} = pessoa
+console.log(sobrenome, test)
+
+const { endereco: {logradouro, numero, cep} } = pessoa
+console.log(logradouro, numero, cep)
